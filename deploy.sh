eddbb97e81a07ad45c7398f776cc7e732ab3e66a
sun <sun@unleashedmind.com>
2013-07-05 23:32:48 +0200

    Revamped script architecture.
    - Cleanly separate configuration from execution; to prevent further unnecessary forks and ensure future maintenance.
    - Introduced new dry-run mode; to verify/test the script before doing potential harm.
    - Improved validations and verifications.
    - General code clean-up.

--- deploy.sh
+++ deploy.sh
@@ -1,99 +1,122 @@
-#! /bin/bash
+#!/bin/bash
 #
-# Script to deploy from Github to WordPress.org Plugin Repository
-# A modification of Dean Clatworthy's deploy script as found here: https://github.com/deanc/wordpress-plugin-git-svn
-# The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
-# Source: https://github.com/thenbrent/multisite-user-management/blob/master/deploy.sh
+# Creates and pushes a git tag to the svn WordPress.org Plugin Directory.
 
-#prompt for plugin slug
-echo -e "Plugin Slug: \c"
-read PLUGINSLUG
+SELFDIR=${BASH_SOURCE%/*}
 
-# main config, set off of plugin slug
-CURRENTDIR=`pwd`
-CURRENTDIR="$CURRENTDIR/$PLUGINSLUG"
-MAINFILE="$PLUGINSLUG.php" # this should be the name of your main php file in the wordpress plugin
+source .wp-release.conf
 
-# git config
-GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
+# Validate configuration.
+[[ -n $SHORTNAME ]] || { echo >&2 "ERROR: Plugin shortname must not be empty."; exit 1; }
+[[ -e $PLUGINPATH ]] || { echo >&2 "ERROR: Configured plugin path does not exist."; exit 1; }
+[[ -e $PLUGINPATH/$MAINFILE ]] || { echo >&2 "ERROR: Configure main plugin file does not exist."; exit 1; }
+[[ -n $SVNUSER ]] || { echo >&2 "ERROR: Subversion username must not be empty."; exit 1; }
 
-# svn config
-SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-SVNURL="http://plugins.svn.wordpress.org/$PLUGINSLUG/" # Remote SVN repo on WordPress.org, with no trailing slash
-SVNUSER="benbalter" # your svn username
+# Initialize variables.
+SVNURL=${SVNURL:="http://plugins.svn.wordpress.org/$SHORTNAME/"}
+SVNCMD="$(which svn) --username=$SVNUSER"
 
-# Let's begin...
-echo ".........................................."
-echo 
-echo "Preparing to deploy WordPress plugin"
-echo 
-echo ".........................................."
-echo 
-
-# Check version in readme.txt is the same as plugin file
-NEWVERSION1=`grep "^Stable tag" $GITPATH/readme.txt | awk -F' ' '{print $3}'`
-echo "readme version: $NEWVERSION1"
-NEWVERSION2=`grep "^Version" $GITPATH/$MAINFILE | awk -F' ' '{print $2}'`
-echo "$MAINFILE version: $NEWVERSION2"
-
-if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Versions don't match. Exiting...."; exit 1; fi
-
-if git show-ref --tags --quiet --verify -- "refs/tags/$NEWVERSION1"
-	then 
-		echo "Version $NEWVERSION1 already exists as git tag. Exiting...."; 
-		exit 1; 
-	else
-		echo "Git version does not exist. Let's proceed..."
+if [[ $DRYRUN != 0 ]]; then
+	DRYRUN="echo +"
+else
+	DRYRUN=
 fi
 
-cd $GITPATH
-echo -e "Enter a commit message for this new version: \c"
-read COMMITMSG
-git commit -am "$COMMITMSG"
+# Output header.
+echo "Project: $SHORTNAME"
+echo "Source:  $PLUGINPATH"
+echo "Target:  $SVNURL"
+echo
 
-echo "Tagging new version in git"
-git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
+# Validate new version.
+NEWVERSION=`grep -E "Version:" "$PLUGINPATH/$MAINFILE" | awk -F' ' '{print $2}'`
+READMEVERSION=`grep "^Stable tag" "$PLUGINPATH/readme.txt" | awk -F' ' '{print $3}'`
 
-echo "Pushing latest commit to origin, with tags"
-git push origin master
-git push origin master --tags
+echo "$MAINFILE version: $NEWVERSION"
+echo "readme.txt version: $READMEVERSION"
 
-echo 
-echo "Creating local copy of SVN repo ..."
-svn co $SVNURL $SVNPATH
+if [ "$NEWVERSION" != "$READMEVERSION" ]; then
+	echo >&2 "ERROR: readme.txt version does not match version in $MAINFILE."
+	exit 1
+fi
 
-echo "Ignoring github specific files and deployment script"
-svn propset svn:ignore "deploy.sh
+if git show-ref --tags --quiet --verify -- "refs/tags/$NEWVERSION"; then
+	echo >&2 "ERROR: git tag $NEWVERSION already exists.";
+	exit 1;
+fi
+
+
+# Review and confirm changes to be committed/released.
+echo "Review and confirm changes to be committed/released:"
+echo
+$DRYRUN git commit -pem "Preparing release $NEWVERSION."
+
+echo
+echo "Tagging release in git..."
+$DRYRUN git tag -a "$NEWVERSION" -m "Tagging release $NEWVERSION."
+
+
+# Safety net; pushing tags to the public is ultimate.
+echo
+echo -e "Ready to publish release to git? (Y/n) [n] \c"
+read CONFIRMED
+[[ $CONFIRMED == "Y" ]] || { echo "Aborted."; exit 1; }
+
+echo
+echo "Pushing master to origin, including tags..."
+$DRYRUN git push origin master --tags
+
+
+# Begin Subversion dump-export process.
+echo
+echo "Checking out SVN repository in $SVNPATH..."
+$SVNCMD checkout $SVNURL $SVNPATH
+
+echo
+echo "Setting svn:ignore for git and release specific files..."
+svn propset svn:ignore "
 README.md
-.git
-.gitignore" "$SVNPATH/trunk/"
+.git*
+.wp-release.conf
+" "$SVNPATH/trunk/"
 
-#export git -> SVN
-echo "Exporting the HEAD of master from git to the trunk of SVN"
-git checkout-index -a -f --prefix=$SVNPATH/trunk/
+echo
+echo "Exporting git HEAD of master to SVN trunk..."
+git checkout-index --all --force --prefix=$SVNPATH/trunk/
 
-#if submodule exist, recursively check out their indexes
-if [ -f ".gitmodules" ]
-then
-echo "Exporting the HEAD of each submodule from git to the trunk of SVN"
-git submodule init
-git submodule update
-git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'
+# Recursively check out submodules, if any.
+if [ -f ".gitmodules" ]; then
+	echo "Exporting git HEAD of submodules to SVN trunk..."
+	git submodule init
+	git submodule update
+	git submodule foreach --recursive 'git checkout-index --all --force --prefix=$SVNPATH/trunk/$path/'
 fi
 
-echo "Changing directory to SVN and committing to trunk"
-cd $SVNPATH/trunk/
-# Add all new files that are not set to be ignored
-svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
-svn commit --username=$SVNUSER -m "$COMMITMSG"
+echo
+echo "Exporting to SVN..."
+pushd $SVNPATH > /dev/null
+pushd trunk > /dev/null
+# Add all new files; svn:ignore files are not listed.
+svn status | grep "^?" | awk '{print $2}' | xargs svn add
 
-echo "Creating new SVN tag & committing it"
-cd $SVNPATH
-svn copy trunk/ tags/$NEWVERSION1/
-cd $SVNPATH/tags/$NEWVERSION1
-svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
+# Extra safety net; SVN does not allow to rewrite history.
+echo
+echo "Changes to be committed to SVN:"
+svn status
+echo
+echo -e "Ready to publish release to SVN? (Y/n) [n] \c"
+read CONFIRMED
+[[ $CONFIRMED == "Y" ]] || { echo "Aborted."; exit 1; }
 
-echo "Removing temporary directory $SVNPATH"
-rm -fr $SVNPATH/
+$DRYRUN $SVNCMD commit -m "Preparing release $NEWVERSION."
 
-echo "*** FIN ***"
+echo
+echo "Creating and committing SVN tag..."
+popd > /dev/null
+svn copy trunk/ tags/$NEWVERSION/
+pushd tags/$NEWVERSION > /dev/null
+$DRYRUN $SVNCMD commit -m "Tagging release $NEWVERSION."
+popd > /dev/null
+
+echo "Done."
+popd > /dev/null
